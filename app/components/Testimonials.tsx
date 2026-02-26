15341f9e0eed4fdbdd78572f97a70802b11d2e80
Harsh Chauhan <harshchauhan@Harshs-MAC.lan>
2026-02-26 15:51:10 +0530

    update tns

--- app/components/Testimonials.tsx
+++ app/components/Testimonials.tsx
@@ -22,7 +22,7 @@ export default function Testimonials() {
     },
     {
       id: 3,
-      name: 'Pavini Jain',
+      name: 'Pavini',
       review: 'The best part about Astromindly is that they consider both psychological patterns and astrological influences. It\'s not just about planets or just about therapy - it\'s about real clarity.'
     },
     {
